c7e27f4c28225004b17db3aac63715f3216700ce
M.V.S. Pavan Kumar <mukkupavankumar6@gmail.com>
2026-06-23 20:14:14 +0530

    Update fifo_sync.sv

--- fifo_sync.sv
+++ fifo_sync.sv
@@ -9,7 +9,15 @@ module fifo_sync #(
     /*
     This is a two stage filp flop synchronizer used to safely pass
     Gray-coded pointers from one block to other[read to write and viceversa]
+
+    How it works?
+        - the input signal generated in one clock domain is sampled by first flipflop that works
+        on destination clk. Since input may change close to sampling edge, the first flipflop can enter metastable state, where
+        output is neither 0 or 1
+        - the second flipflop samples the output of first one on next destination clock edge, giving firstone an
+        entire clock cycle to resolve meta stability
     */
+    
     logic[WIDTH-1 : 0] q1;
 
     always_ff @(posedge clk or posedge rst) begin
